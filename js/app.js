6565cdff98112ad5dacf0511a63a92b8044bb82d
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-02-12 13:50:32 -0800

    added image rendering to app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,31 @@
 'use strict'
 
-let Weapon_List = ['staff', 'sword', 'hammer', 'dagger']
+let Dice = Math.random();
+console.log(Dice);
 
 function Character(Character_Name, Weapon) {
     this.Character_Name = Character_Name,
-    this.Weapon = Weapon;
+    this.Weapon = Weapon
 }
 
-const Hajduk = Character('Hajduk', Weapon_List[0]);
-const Kahuna = Character('Kahuna', Weapon_List[1]);
-const Mamluk = Character('Mamluk', Weapon_List[2]);
-const Admir = Character('Admir', Weapon_List[3]);
+let Game_Image_Array = ['img/images/admir.jpg', 'img/images/bkg-1,jpg', 'img/images/bkg-2.jpg', 'img/images/bkg-3.jpg', 'img/images/bkg-4.jpg', 'img/images/hajduk.jpg', 'img/images/mamluk.jpg', 'img/images/sulta.jpg', 'img/images/tabib.png']; //Array for the image files
+let Weapons_Array = ['staff', 'sword', 'hammer', 'dagger'] // Array for the weapons
+
+function Render_Image() {
+    let Image_Container = document.getElementById('dynamic-image');
+    let Image = document.createElement('img');
+    Image.src = Game_Image_Array[3];
+    Image.alt = 'Test';
+
+    Image_Container.appendChild(Image);
+
+}
+
+const Hajduk = new Character('Hajduk', Weapons_Array[0]);
+const Kahuna = new Character('Kahuna', Weapons_Array[1]);
+const Mamluk = new Character('Mamluk', Weapons_Array[2]);
+const Admir = new Character('Admir', Weapons_Array[3]);
+
 console.log(Hajduk, Kahuna, Mamluk, Admir);
+
+Render_Image();
