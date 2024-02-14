c97be84baa6068f6eb4fa0fee06cae6a1b4ec900
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-02-14 09:39:21 -0800

    fixing a merge issue

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 'use strict'
 
-let Dice = Math.random();
-console.log(Dice);
+function Dice(min, max){
+    let minCeil = Math.ceil(min);
+    let maxFloor = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloor - minCeil + 1) + minCeil);
+}
 
-function Character(Character_Name, Weapon, HealthPoints, Gold, xp, Player_Level) {
+console.log(Dice(1, 6));
+
+function Character(Character_Name, Weapon, Health_Points, Gold, xp, Player_Level) {
     this.Character_Name = Character_Name,
     this.Weapon = Weapon
-    this.HealthPoints = HealthPoints;
+    this.Health_Points = Health_Points;
     this.Gold = Gold;
     this.xp = xp;
     this.Player_Level = Player_Level;
+}
+
+function Monster(Monster_Name, Health_Points, Gold, Monster_Level) {
+    this.Monster_Name = Monster_Name;
+    this.Health_Points = Health_Points;
+    this.Gold = Gold;
+    this.Monster_Level = Monster_Level;
 }
 
 let Weapons_Array = ['staff', 'sword', 'hammer', 'dagger'] // Array for the weapons
@@ -27,10 +39,11 @@ function Render_Image(image) {
 }
 
 function Render_Buttons() {
+    let Button_Functions = ['Attack', 'Dodge', 'Run'];
     let Button_Field = document.getElementById('game-buttons');
-    for(let x = 1; x < 4; x++) {
+    for(let x = 0; x < Button_Functions.length; x++) {
         let Button = document.createElement('button');
-        Button.textContent = 'Test: ' + x;
+        Button.textContent = Button_Functions[x];
         Button_Field.appendChild(Button);
     }
 }
